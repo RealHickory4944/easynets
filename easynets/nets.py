d507d578c39cc12d57ad60b6fcd7f59b8345d2bf
Theobobble <theomatzen@gmail.com>
2025-10-28 19:55:38 -0700

    Update nets.py
    maybe?

--- easynets/nets.py
+++ easynets/nets.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 def create_network(num_layers, layers, activation):
   for i in range(num_layers - 1):
     if i == 0:
-      x = [(tf.keras.layers.Dense(layers[1], activation=activation[i], input_shape=layers[0]))]
+      x = [(tf.keras.layers.Dense(layers[1], activation=activation[i], input_shape=(layers[0],)))]
     else:
       x.append(tf.keras.layers.Dense(layers[i + 1], activation=activation[i]))
   x = tf.keras.models.Sequential(x)
