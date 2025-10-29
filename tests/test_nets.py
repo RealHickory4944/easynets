e1b12523922043f0bfc1682cd3b8201469275efa
Theobobble <theomatzen@gmail.com>
2025-10-28 19:55:11 -0700

    Update test_nets.py
    i think i gotta do more tho.

--- tests/test_nets.py
+++ tests/test_nets.py
@@ -1,6 +1,17 @@
+import pytest
 import easynets as en
+import tensorflow as tf
 
 def test_create_network():
-  en.nets.create_network(num_layers=4,
-                         layers=[2, 5, 5, 1], # input and output included
-                         activation=['relu', 'relu', 'sigmoid'] # only hidden and output
+  net = en.nets.create_network(num_layers=4,
+                               layers=[2, 5, 5, 1], # input and output included
+                               activation=['relu', 'relu', 'sigmoid']) # only hidden and output
+  expected = tf.keras.models.Sequential([
+    tf.keras.layers.Dense(5, activation='relu', input_shape=(2,)),
+    tf.keras.layers.Dense(5, activation='relu')
+    tf.keras.layers.Dense(1, activation='sigmoid')
+  ])
+  expected.compile(optimizer='adam',
+                   loss='mse',
+                   metrics=['accuracy'])
+  assert net is expected
