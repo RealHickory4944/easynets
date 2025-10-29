4a7199fe1dcf6ff1c57a844fc9b8452e4ecc2651
Theobobble <theomatzen@gmail.com>
2025-10-28 20:49:37 -0700

    Update nets.py
    i gotta sleep

--- easynets/nets.py
+++ easynets/nets.py
@@ -6,7 +6,7 @@ def create_network(num_layers, layers, activation):
       x = [(tf.keras.layers.Dense(layers[1], activation=activation[i], input_shape=(layers[0],)))]
     else:
       x.append(tf.keras.layers.Dense(layers[i + 1], activation=activation[i]))
-  x = Model(x)
+  x = tf.keras.models.Sequential(x)
   x.compile(optimizer='adam',
             loss='mse',
             metrics=['accuracy'])
