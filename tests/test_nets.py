876b72acf5e2ee656d922b6f7e5c96c6c5c946e7
Theobobble <theomatzen@gmail.com>
2025-10-28 19:47:00 -0700

    Update test_nets.py

--- tests/test_nets.py
+++ tests/test_nets.py
@@ -1,6 +1,6 @@
 import easynets as en
 
-def test_create_network(
-  num_layers=5,
-  layers=[2, 5, 5, 1], # input and output included
-  activation=['relu', 'relu', 'relu', 'sigmoid'])
+def test_create_network():
+  en.nets.create_network(num_layers=4,
+                         layers=[2, 5, 5, 1], # input and output included
+                         activation=['relu', 'relu', 'sigmoid'] # only hidden and output
